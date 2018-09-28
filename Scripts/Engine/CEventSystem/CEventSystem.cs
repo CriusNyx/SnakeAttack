e0400b6574bfaa041e22e01fee6421f3b2518509
WINDOWS-US54VFC\RJ 1050 <rjreynoldsw@gmail.com>
2018-09-28 13:17:01 -0400

    Added CEventSystem

---
+++ Scripts/Engine/CEventSystem/CEventSystem.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+/// <summary>
+/// Custom Event System
+///     The custom even system, is responcible for broadcasting events to the game engine.
+///     This pattern allows the state of the game engine to be updated when the receiver of the messages are not known
+///     For example, An input controller class may be responcible for getting input from all key presses, and gamepad inputs.
+///         The input controller will broadcast all of it's inputs into the event system.
+///         The character controller class will be an event handler, and register itself with the event system.
+///         Because it's register on the same channel as the input controller, it will receive all events from the input controller.
+/// </summary>
+public class CEventSystem : MonoBehaviour
+{
+
+
+    /// <summary>
+    /// Gets, or creates an instance of the event system
+    /// Event system was implemented as a monobehaviour so that the event system would reset itself whenever a new scene is loaded
+    /// </summary>
+    private static CEventSystem Instance
+    {
+        get
+        {
+            if(instance == null)
+                instance = FindObjectOfType<CEventSystem>();
+            if(instance == null)
+                instance = new GameObject("Event System").AddComponent<CEventSystem>();
+            return instance;
+        }
+    }
+    /// <summary>
+    /// A private reference to the current event system. This should only be accessed by Instance
+    /// </summary>
+    private static CEventSystem instance;
+
+    /// <summary>
+    /// Sum up the number of event handlers currently in the system.
+    /// </summary>
+    public static int Count
+    {
+        get
+        {
+            var output = 0;
+            var dic = Instance.eventObjects;
+            foreach(var subdic in dic)
+            {
+                foreach(var list in subdic.Value)
+                {
+                    foreach(var eh in list.Value)
+                    {
+                        output += 1;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+
+    /// <summary>
+    /// The datastructure containing the events in the event system
+    /// </summary>
+    private Dictionary
+        <Enum,
+        Dictionary
+            <Enum,
+            List<ICEventHandler>>>
+                eventObjects = new Dictionary<Enum, Dictionary<Enum, List<ICEventHandler>>>();
+
+    /// <summary>
+    /// Search the event system for the requested list
+    /// </summary>
+    /// <param name="category"></param>
+    /// <param name="subcategory"></param>
+    /// <returns></returns>
+    private static List<ICEventHandler> GetHandlerList(Enum category, Enum subcategory, bool create = false)
+    {
+        var eventObjects = instance.eventObjects;
+        if(!eventObjects.ContainsKey(category))
+        {
+            if(!create)
+                return null;
+            eventObjects[category] = new Dictionary<Enum, List<ICEventHandler>>();
+        }
+        var cat = eventObjects[category];
+        if(!cat.ContainsKey(category))
+        {
+            if(!create)
+                return null;
+            cat[subcategory] = new List<ICEventHandler>();
+        }
+        return cat[subcategory];
+    }
+
+    /// <summary>
+    /// Add a new event handler to the system
+    /// Once the handler is added, it will recieve all events which are boradcasted on the system.
+    /// If the object has already been registered in this list, this class will trhow an error.
+    /// </summary>
+    /// <param name="catagory"></param>
+    /// <param name="subcatagory"></param>
+    /// <param name="eventHandler"></param>
+    public static void AddEventHandler(Enum catagory, Enum subcatagory, ICEventHandler eventHandler)
+    {
+        var handlerList = GetHandlerList(catagory, subcatagory, true);
+        if(handlerList.Contains(eventHandler))
+        {
+            throw new System.Exception("The event handler is already in this catagory in the event system");
+        }
+        handlerList.Add(eventHandler);
+    }
+
+    /// <summary>
+    /// Remove an event handler from the system.
+    /// It's important to make sure all event handler are removed when they are disposed of.
+    /// Event handlers left in the system, will cause memory leaks, or errors.
+    /// </summary>
+    /// <param name="catagory"></param>
+    /// <param name="subcatagory"></param>
+    /// <param name="eventHandler"></param>
+    public static void RemoveEventHandler(Enum catagory, Enum subcatagory, ICEventHandler eventHandler)
+    {
+        var handlerList = GetHandlerList(catagory, subcatagory);
+        if(handlerList != null)
+            handlerList.Remove(eventHandler);
+    }
+
+    /// <summary>
+    /// Broadcast an event into the event system.
+    /// </summary>
+    /// <param name="catagory"></param>
+    /// <param name="subcatagory"></param>
+    /// <param name="e"></param>
+    public static void BroadcastEvent(Enum catagory, Enum subcatagory, CEvent e)
+    {
+        var handlerList = GetHandlerList(catagory, subcatagory);
+        if(handlerList != null)
+            foreach(var handler in handlerList)
+                handler.AcceptEvent(e);
+    }
+}
+
+/// <summary>
+/// Inherit from this class to create a new event system event.
+/// This should be done in the class which needs to interface with the event system (usually the handler)
+/// </summary>
+public abstract class CEvent
+{
+
+}
+
+/// <summary>
+/// Implement this interface on your class to allow it to be added to the event system.
+/// </summary>
+public interface ICEventHandler
+{
+    void AcceptEvent(CEvent e);
+}
---
+++ Tests/EventSystemBaseTest.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class EventSystemBaseTest {
+
+    /// <summary>
+    /// Verify the integrety of the event system
+    /// </summary>
+    [Test]
+    public void EventSystemBaseTestSimplePasses()
+    {
+        //Check that the event system is empty
+        Assert.True(CEventSystem.Count == 0);
+
+        //create an event, add it to the system, and check the count
+        TestEventObject testEvent = new TestEventObject();
+        CEventSystem.AddEventHandler(category.category, category.category, testEvent);
+        Assert.True(CEventSystem.Count == 1);
+
+        //Check the state of the test class after broadcasting a set true, set false, and foo event
+        Assert.True(!testEvent.State);
+        CEventSystem.BroadcastEvent(category.category, category.category, new SetTrueEvent());
+        Assert.True(testEvent.State);
+        CEventSystem.BroadcastEvent(category.category, category.category, new SetFalseEvent());
+        Assert.True(!testEvent.State);
+        CEventSystem.BroadcastEvent(category.category, category.category, new FooEvent());
+        Assert.True(!testEvent.State);
+
+        //remove the event, check the count of the system.
+        CEventSystem.RemoveEventHandler(category.category, category.category, testEvent);
+        Assert.True(CEventSystem.Count == 0);
+    }
+
+    private class TestEventObject : ICEventHandler
+    {
+        private bool state = false;
+        public bool State
+        {
+            get
+            {
+                return state;
+            }
+        }
+
+        public void AcceptEvent(CEvent e)
+        {
+            if(e is SetFalseEvent)
+                state = false;
+            else if(e is SetTrueEvent)
+                state = true;
+        }
+    }
+
+    private class SetTrueEvent : CEvent
+    {
+
+    }
+
+    private class SetFalseEvent : CEvent
+    {
+
+    }
+
+    private class FooEvent : CEvent
+    {
+
+    }
+
+    private enum category
+    {
+        category,
+    }
+}
