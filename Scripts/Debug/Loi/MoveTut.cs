90d29085b44be5f12e519277b5c6538be607f543
Loi Huynh <loihuynh03@gmail.com>
2018-10-11 18:34:29 -0400

    CUPID SHUFFLE

--- Scripts/Debug/Loi/MoveTut.cs
+++ Scripts/Debug/Loi/MoveTut.cs
@@ -4,12 +4,44 @@ using UnityEngine;
 
 public class MoveTut : MonoBehaviour {
 
-	// Use this for initialization
+	public static IEnumerable<GridNode> CreateAGrid()
+    {
+        //initialize the array for linking, and the list for output
+        int width = 100, height = 100;
+        GridNode[,] gridNodes = new GridNode[width, height];
+        List<GridNode> output = new List<GridNode>();
+
+        //loop through each node
+        for(int x =0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                //crete a new grid node
+                //create takes two params, position and name
+                GridNode node = GridNode.Create(new Vector3(x, y, 0), "GridNode:(" + x + "," + y + ")");
+                //put the grid node in the array
+                gridNodes[x, y] = node;
+                //put the grid node in the output list
+                output.Add(node);
+
+            }
+        }
+        //links the nodes
+        GridNode.AutoLink(gridNodes);
+        //return the list
+        return output;
+
+    }
+
+
+
+
+
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
 		
 	}
---
+++ Scripts/Debug/Loi/gridTut.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class gridTut : MonoBehaviour {
+    private GridTransform gridTransform;
+    private List<GridNode> grid;
+    // Use this for initialization
+    private void Start () {
+        grid = MoveTut.CreateAGrid().ToList();
+        gridTransform = gameObject.AddComponent<GridTransform>();
+
+        gridTransform.Warp(grid[0]);
+
+    }
+	
+	// Update is called once per frame
+	private void Update () {
+        GridNode currentNode = gridTransform.CurrentNode;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            //move right
+            gridTransform.MoveTo(currentNode.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            //move left
+            gridTransform.MoveTo(currentNode.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //move up
+            gridTransform.MoveTo(currentNode.top);
+        }
+
+
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            //move down
+            gridTransform.MoveTo(currentNode.bottom);
+        }
+        //move the 3d position to grid positions
+        transform.position = gridTransform.Target;
+	}
+}
+
+
