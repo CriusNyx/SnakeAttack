3e3f0d2fbc4adae1f3a05a71f4f79d3a09e9f07c
Olivia Yee <olyee@oakland.edu>
2018-10-25 17:25:39 -0400

    syncing with master branch

---
+++ Scripts/Debug/olivia/CameraControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    ///tips and things to remember
+    ///attach to player to control camera
+    ///get player position using Player.Instance
+    ///but use null check to prevent race condition with player and camera
+    private void Awake()
+    {
+
+    }
+
+}
