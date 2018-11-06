97a93f0c4b12c95338b449ffdae6b0d17e5daecb
nhaberland <nhaberland@oakland.edu>
2018-11-06 10:35:21 -0500

    Sound

---
+++ Scripts/Debug/Nathan/Sound_test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/Garbage");
+            GameObject instance = GameObject.Instantiate(prefab);
+            instance.AddComponent<Sound_DestroyWhenDone>(); 
+        }
+	}
+}
---
+++ Scripts/Engine/Sound/Sound_DestroyWhenDone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_DestroyWhenDone : MonoBehaviour {
+    AudioSource source;
+	// Use this for initialization
+	void Start () {
+        source = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!source.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
