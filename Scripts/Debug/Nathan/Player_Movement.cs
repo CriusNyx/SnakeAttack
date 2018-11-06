260a3d3c6a2fdb6ba0c0833f17f60988e2754d29
nhaberland <nhaberland@oakland.edu>
2018-11-06 09:56:18 -0500

    Update

--- Scripts/Debug/Nathan/Player_Movement.cs
+++ Scripts/Debug/Nathan/Player_Movement.cs
@@ -2,18 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Movement : MonoBehaviour {
+public class Player_Movement : MonoBehaviour
+{
     //Nathan Haberland
-    private GridTransform gridTransform;
-    private List<GridNode> grid;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        
-	}
+    private GridTransform gridTransform;
+    private List<Player_Tail> tail = new List<Player_Tail>();
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gridTransform = gameObject.AddComponent<GridTransform>();
+        gridTransform.Warp(GridSystem.GetNode(0, 0));
+
+
+        var tweener = gameObject.AddComponent<LinearTweener>();
+        tweener.speed = 20;
+        tweener.autoTarget = () =>
+        {
+            return gridTransform.Target;
+        };
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(Direction.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Grow();
+        }
+    }
+
+
+
+
+
+    bool Move(Direction dir)
+    {
+        return gridTransform.Move(dir);
+    }
+
+    void Grow()
+    {
+        var tailpiece = Player_Tail.Create(gridTransform, gridTransform.CurrentNode);
+        tail.Add(tailpiece);
+    }
 }
---
+++ Scripts/Debug/Nathan/Player_Tail.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Tail : MonoBehaviour {
+
+    GridTransform gridTransform;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private GridTransform leader;
+        
+    public static Player_Tail Create(GridTransform leader, GridNode position)
+    {
+        return GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<Player_Tail>().Init(leader, position);
+    }
+    
+    private Player_Tail Init(GridTransform leader, GridNode position)
+    {
+        gridTransform = gameObject.AddComponent<GridTransform>();
+        gridTransform.Warp(position);
+        transform.position = gridTransform.Target;
+        this.leader = leader;
+        return this;
+    }
+
+}
