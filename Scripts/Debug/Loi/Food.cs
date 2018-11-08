115e39627023e57d751609b76f2eebf4dcca8eb3
Loi Huynh <loihuynh03@gmail.com>
2018-11-08 17:50:04 -0500

    Add files via upload

---
+++ Scripts/Debug/Loi/Food.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+
+    //Food Prefab
+    public GameObject foodPrefab;
+
+    // Borders
+    public Transform border_top;
+    public Transform border_bottom;
+    public Transform border_left;
+    public Transform border_right;
+
+    //initialization
+    void Start()
+    {
+        InvokeRepeating("Spawn", 3, 4);
+    }
+    void Spawn()
+    {
+        // x position between left & right border
+        int x = (int)Random.Range(border_left.position.x,
+                                  border_right.position.x);
+
+        // y position between top & bottom border
+        int y = (int)Random.Range(border_bottom.position.y,
+                                  border_top.position.y);
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefab,
+                    new Vector2(x, y),
+                    Quaternion.identity); // default rotation
+    }
+}
---
+++ Scripts/Debug/Loi/Snake.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+    bool ate = false;
+    public GameObject tailPrefab;
+    List<Transform> tail = new List<Transform>();
+    Vector3 dir = Vector3.right;
+    void Start()
+    {
+        InvokeRepeating("Move", 0.3f, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move in a new Direction?
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector3.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = -Vector3.up;    // '-up' means 'down'
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = -Vector3.right; // '-right' means 'left'
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector3.up;
+
+
+    }
+    void Move()
+    {
+        // Save current position (gap will be here)
+        Vector3 v = transform.position;
+
+        // Move head into new direction (now there is a gap)
+        transform.Translate(dir);
+
+        // Ate something? Then insert new Element into gap
+        if (ate)
+        {
+            // Load Prefab into the world
+            GameObject g = (GameObject)Instantiate(tailPrefab,
+                                                  v,
+                                                  Quaternion.identity);
+
+            // Keep track of it in our tail list
+            tail.Insert(0, g.transform);
+
+            // Reset the flag
+            ate = false;
+        }
+        // Do we have a Tail?
+        else if (tail.Count > 0)
+        {
+            // Move last Tail Element to where the Head was
+            tail.Last().position = v;
+
+            // Add to front of list, remove from the back
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+    void OnTriggerEnter(Collider coll)
+    {
+        // Food?
+        if (coll.name.StartsWith("FoodPrefab"))
+        {
+            // Get longer in next Move call
+            ate = true;
+
+            // Remove the Food
+            Destroy(coll.gameObject);
+        }
+        // Collided with Tail or Border
+        else
+        {
+            // ToDo 'You lose' screen
+        }
+    }
+}
