e868d2263f471bd7a52a0783a340fe9c9520f006
WINDOWS-US54VFC\RJ 1050 <rjreynoldsw@gmail.com>
2018-11-29 18:58:50 -0500

    Level Transitions

--- Scripts/Engine/GameState/GameState.cs
+++ Scripts/Engine/GameState/GameState.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameState : MonoBehaviour, ICEventHandler
 {
     int enemyCount = 0;
     public int snakeGrowCount = 5;
+    public string nextLevel = "";
     public static int SnakeGrowCount
     {
         get
@@ -29,9 +31,34 @@ public class GameState : MonoBehaviour, ICEventHandler
             enemyCount--;
             if(enemyCount == 0)
             {
-                Debug.Log("Level Complete");
+                CompleteLevel();
             }
         }
+    }
+
+    public void CompleteLevel()
+    {
+        GameObject levelCompleteText = new GameObject("Level_Complete_Text");
+        TextMesh text = levelCompleteText.AddComponent<TextMesh>();
+        text.fontSize = 100;
+        text.characterSize = 0.02f;
+        text.anchor = TextAnchor.MiddleCenter;
+        text.text = "Level Complete";
+        text.alignment = TextAlignment.Center;
+        text.transform.SetParent(GameObject.Find("Main Camera").transform, false);
+        text.transform.localPosition = Vector3.forward * 1f;
+
+        StartCoroutine(LoadLevel());
+    }
+
+    private IEnumerator LoadLevel()
+    {
+        float time = Time.time + 1f;
+        while(Time.time < time)
+        {
+            yield return null;
+        }
+        SceneManager.LoadScene(nextLevel);
     }
 
     public class EnemySpawnEvent : CEvent
