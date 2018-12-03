97502ce43a0707a1ebbe87b03e97b031995db028
nhaberland <nhaberland@oakland.edu>
2018-12-02 20:31:39 -0500

    Main Menu

---
+++ Scripts/Debug/Nathan/LoadSceneOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+    public void LoadByIndex(string sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    /*This code was pulled from Unity's "Creating a Main Menu" Video Tutorial
+     * Author: Unity Team
+     * Date: September 5th, 2016
+     * Availability: https://unity3d.com/learn/tutorials/topics/user-interface-ui/creating-main-menu
+    */
+}
+
---
+++ Scripts/Debug/Nathan/QuitOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour
+{
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+
+
+#endif
+    }
+    /*This code was pulled from Unity's "Creating a Main Menu" Video Tutorial
+     * Author: Unity Team
+     * Date: September 5th, 2016
+     * Availability: https://unity3d.com/learn/tutorials/topics/user-interface-ui/creating-main-menu
+    */
+}
